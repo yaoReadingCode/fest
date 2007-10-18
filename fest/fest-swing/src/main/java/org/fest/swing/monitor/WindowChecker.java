a66eb70e749e3504b8eda5e62e35e1f29bbca878
Alex.Ruiz.05 <Alex.Ruiz.05@b5e4cadd-ff2d-0410-b0d3-97d37c325e25>
2007-10-18 06:08:56 +0000

    More work on window event monitors.

---
+++ fest/fest-swing/src/main/java/org/fest/swing/monitor/WindowChecker.java
@@ -0,0 +1,92 @@
+/*
+ * Created on Oct 17, 2007
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ * 
+ * Copyright @2007 the original author or authors.
+ */
+package org.fest.swing.monitor;
+
+import java.awt.AWTException;
+import java.awt.Insets;
+import java.awt.Robot;
+import java.awt.Window;
+
+import javax.swing.SwingUtilities;
+
+import static java.lang.Math.max;
+
+import static org.fest.swing.util.AWT.insetsFrom;
+
+/**
+ * Understands verification of the state of a window.
+ * <p>
+ * Adapted from <code>abbot.tester.WindowTracker</code> from <a href="http://abbot.sourceforge.net"
+ * target="_blank">Abbot</a>.
+ * </p>
+ *
+ * @author Alex Ruiz
+ */
+class WindowChecker {
+
+  private static int sign = 1;
+
+  private final Windows windows;
+  private Robot robot;
+  
+  WindowChecker(Windows windows) {
+    this.windows = windows;
+    createRobot();
+  }
+
+  private void createRobot() {
+    try {
+      robot = new java.awt.Robot();
+    } catch (AWTException e) {}
+  }
+
+  /**
+   * Checks whether the given window is ready for input.
+   * @param w the given window.
+   */
+  void checkIfReady(Window w) {
+    if (robot == null) return;
+    // Must avoid frame borders, which are insensitive to mouse motion (at least on w32).
+    Insets insets = insetsFrom(w);
+    int width = w.getWidth();
+    int height = w.getHeight();
+    int x = w.getX() + insets.left + (width - (insets.left + insets.right)) / 2;
+    int y = w.getY() + insets.top + (height - (insets.top + insets.bottom)) / 2;
+    if (x != 0 && y != 0) {
+      robot.mouseMove(x, y);
+      if (width > height) robot.mouseMove(x + sign, y);
+      else robot.mouseMove(x, y + sign);
+      sign = -sign;
+    }
+    if (windows.isShowingButNotReady(w) && isEmptyFrame(w)) 
+      makeLargeEnoughToReceiveEvents(w, insets, width, height);
+  }
+
+  private void makeLargeEnoughToReceiveEvents(final Window w, Insets insets, int width, int height) {
+    final int newWidth = max(width, insets.left + insets.right + 3);
+    final int newHeight = max(height, insets.top + insets.bottom + 3);
+    SwingUtilities.invokeLater(new Runnable() {
+      public void run() {
+        w.setSize(newWidth, newHeight);
+      }
+    });
+  }
+  
+  private boolean isEmptyFrame(Window w) {
+    Insets insets = insetsFrom(w);
+    return insets.top + insets.bottom == w.getHeight() || insets.left + insets.right == w.getWidth();
+  }
+}
---
+++ fest/fest-swing/src/main/java/org/fest/swing/monitor/WindowMonitor.java
@@ -0,0 +1,116 @@
+/*
+ * Created on Oct 8, 2007
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ * 
+ * Copyright @2007 the original author or authors.
+ */
+package org.fest.swing.monitor;
+
+import java.awt.Component;
+import java.awt.EventQueue;
+import java.awt.Frame;
+import java.awt.Window;
+import java.util.Collection;
+
+import static org.fest.swing.monitor.ContextMonitor.attachContextMonitor;
+import static org.fest.swing.monitor.WindowAvailabilityMonitor.attachWindowAvailabilityMonitor;
+import static org.fest.swing.monitor.WindowVisibilityMonitor.attachWindowVisibilityMonitor;
+
+/**
+ * Understands a monitor that keeps track of all known root windows (showing, hidden, closed.)
+ * <p>
+ * Adapted from <code>abbot.tester.WindowTracker</code> from <a href="http://abbot.sourceforge.net"
+ * target="_blank">Abbot</a>.
+ * </p>
+ * 
+ * @author Alex Ruiz
+ */
+final class WindowMonitor {
+
+  private final Windows windows = new Windows();
+  private final Context context = new Context();
+  private final WindowChecker windowChecker = new WindowChecker(windows);
+
+  /**
+   * Create an instance of WindowTracker which will track all windows coming and going on the current and subsequent app
+   * contexts. WARNING: if an applet loads this class, it will only ever see stuff in its own app context.
+   */
+  WindowMonitor() {
+    attachContextMonitor(windows, context);
+    attachWindowAvailabilityMonitor(windows);
+    populateExistingWindows();
+  }
+
+  private void populateExistingWindows() {
+    for (Frame f : Frame.getFrames()) examine(f);
+  }
+
+  private void examine(Window w) {
+    attachWindowVisibilityMonitor(w, windows);
+    for (Window owned : w.getOwnedWindows()) examine(owned);
+    windows.markExisting(w);
+    context.addContextFor(w);
+  }
+
+  /**
+   * Returns whether the window is ready to receive OS-level event input. A window's "isShowing" flag may be set true
+   * before the WINDOW_OPENED event is generated, and even after the WINDOW_OPENED is sent the window peer is not
+   * guaranteed to be ready.
+   * @param w the given window.
+   * @return whether the window is ready to receive OS-level event input.
+   */
+  public boolean isWindowReady(Window w) {
+    if (windows.isReady(w)) return true;
+    windowChecker.checkIfReady(w);
+    return false;
+  }
+
+  /**
+   * Returns the event queue corresponding to the given component. In most cases, this is the same as
+   * Component.getToolkit().getSystemEventQueue(), but in the case of applets will bypass the AppContext and provide the
+   * real event queue.
+   * @param c the given component.
+   * @return the event queue corresponding to the given component.
+   */
+  public EventQueue queueFor(Component c) {
+    return context.eventQueueFor(c);
+  }
+
+  /**
+   * Returns all known event queues.
+   * @return all known event queues.
+   */
+  public Collection<EventQueue> allEventQueues() {
+    return context.allEventQueues();
+  }
+
+  /**
+   * Return all available root windows. A root window is one that has a null parent. Nominally this means a list similar
+   * to that returned by <code>{@link Frame#getFrames() Frame.getFrames()}</code>, but in the case of an
+   * <code>{@link java.applet.Applet}</code> may return a few dialogs as well.
+   * @return all available root windows.
+   */
+  public Collection<Component> rootWindows() { 
+    return context.rootWindows();
+  }
+
+  /**
+   * Returns the singleton instance of this class.
+   * @return the singleton instance of this class.
+   */
+  public static WindowMonitor instance() {
+    return SingletonHolder.INSTANCE;
+  }
+
+  private static class SingletonHolder {
+    static final WindowMonitor INSTANCE = new WindowMonitor();
+  }
+}
