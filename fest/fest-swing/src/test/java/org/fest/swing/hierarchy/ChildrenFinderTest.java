486fc6231482960a0f30984e10450e8acffe8503
Alex.Ruiz.05 <Alex.Ruiz.05@b5e4cadd-ff2d-0410-b0d3-97d37c325e25>
2007-10-27 02:02:56 +0000

    More work on window event monitors.

---
+++ fest/fest-swing/src/test/java/org/fest/swing/hierarchy/ChildrenFinderTest.java
@@ -0,0 +1,68 @@
+/*
+ * Created on Oct 26, 2007
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ * 
+ * Copyright @2007 the original author or authors.
+ */
+package org.fest.swing.hierarchy;
+
+import java.awt.Component;
+import java.awt.Container;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JDesktopPane;
+
+import static org.fest.assertions.Assertions.assertThat;
+import static org.fest.util.Collections.list;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/**
+ * Tests for <code>{@link ChildrenFinder}</code>.
+ *
+ * @author Yvonne Wang
+ * @author Alex Ruiz
+ */
+public class ChildrenFinderTest {
+
+  private ChildrenFinder finder;
+  
+  private JDesktopPaneChildrenFinder desktopPaneChildrenFinder;
+  private JMenuChildrenFinder menuChildrenFinder;
+  private WindowChildrenFinder windowChildrenFinder;
+  
+  @BeforeMethod public void setUp() {
+    finder = new ChildrenFinder();
+    desktopPaneChildrenFinder = new JDesktopPaneChildrenFinder();
+    menuChildrenFinder = new JMenuChildrenFinder();
+    windowChildrenFinder = new WindowChildrenFinder();
+  }
+  
+  @Test public void shouldReturnIconifiedInternalFramesIfComponentIsJDesktopPane() throws Exception {
+    MDIFrame frame = MDIFrame.show(getClass());
+    frame.internalFrame().setIcon(true);
+    JDesktopPane desktop = frame.desktop();
+    assertThat(finder.childrenOf(desktop)).containsOnly(childrenOf(desktop));
+    frame.beDisposed();
+  }
+  
+  private Object[] childrenOf(Container c) {
+    List<Component> children = new ArrayList<Component>();
+    children.addAll(list(c.getComponents()));
+    children.addAll(desktopPaneChildrenFinder.nonExplicitChildrenOf(c));
+    children.addAll(menuChildrenFinder.nonExplicitChildrenOf(c));
+    children.addAll(windowChildrenFinder.nonExplicitChildrenOf(c));
+    return children.toArray();
+  }
+}
