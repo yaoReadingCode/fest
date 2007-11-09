b1c97550f1beb7bd9d3cf0c2748256eda430326f
Alex.Ruiz.05 <Alex.Ruiz.05@b5e4cadd-ff2d-0410-b0d3-97d37c325e25>
2007-11-09 04:39:09 +0000

    Minor changes.

--- fest/fest-assert/src/main/java/org/fest/assertions/ByteArrayAssert.java
+++ fest/fest-assert/src/main/java/org/fest/assertions/ByteArrayAssert.java
@@ -1,5 +1,5 @@
 /*
- * Created on Oct 26, 2007
+ * Created on Nov 8, 2007
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
