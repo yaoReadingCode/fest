6531c626cea9b3f29ac80154d569df6f75e3a572
wanghy1101 <wanghy1101@b5e4cadd-ff2d-0410-b0d3-97d37c325e25>
2007-12-23 23:53:51 +0000

    Complete - task 78: [fest-assert] StringAssert enhancements http://code.google.com/p/fest/issues/detail?id=78
    Added method 'excludes' to StringAssert.

--- fest/fest-assert/src/main/java/org/fest/assertions/StringAssert.java
+++ fest/fest-assert/src/main/java/org/fest/assertions/StringAssert.java
@@ -1,29 +1,29 @@
 /*
  * Created on Dec 26, 2006
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
- * 
+ *
  * Copyright @2006 the original author or authors.
  */
 package org.fest.assertions;
-
-import org.fest.util.Strings;
 
 import static org.fest.assertions.Fail.fail;
 import static org.fest.assertions.Formatting.format;
 import static org.fest.util.Strings.concat;
 import static org.fest.util.Strings.quote;
 
+import org.fest.util.Strings;
+
 /**
- * Understands assertion methods for <code>String</code>s. To create a new instance of this class use the 
+ * Understands assertion methods for <code>String</code>s. To create a new instance of this class use the
  * method <code>{@link Assertions#assertThat(String)}</code>.
  *
  * @author Yvonne Wang
@@ -33,7 +33,7 @@ public final class StringAssert extends GroupAssert<String> {
   StringAssert(String actual) {
     super(actual);
   }
-  
+
   /**
    * Sets the description of the actual value, to be used in as message of any <code>{@link AssertionError}</code>
    * thrown when an assertion fails.
@@ -45,7 +45,7 @@ public final class StringAssert extends GroupAssert<String> {
   }
 
   /**
-   * Alternative to <code>{@link #as(String)}</code>, since "as" is a keyword in 
+   * Alternative to <code>{@link #as(String)}</code>, since "as" is a keyword in
    * <a href="http://groovy.codehaus.org/" target="_blank">Groovy</a>.
    * @param description the description of the actual value.
    * @return this assertion object.
@@ -53,9 +53,9 @@ public final class StringAssert extends GroupAssert<String> {
   public StringAssert describedAs(String description) {
     return as(description);
   }
-  
+
   /**
-   * Verifies that the actual <code>String</code> satisfies the given condition. 
+   * Verifies that the actual <code>String</code> satisfies the given condition.
    * @param condition the condition to satisfy.
    * @return this assertion object.
    * @throws AssertionError if the actual <code>String</code> does not satisfy the given condition.
@@ -69,18 +69,18 @@ public final class StringAssert extends GroupAssert<String> {
    * @throws AssertionError if the actual <code>String</code> is not <code>null</code> or not empty.
    */
   public void isEmpty() {
-    if (!Strings.isEmpty(actual)) 
+    if (!Strings.isEmpty(actual))
       fail(concat(format(description()), "the String ", quote(actual), " should be empty or null"));
   }
-  
+
   /**
    * Verifies that the actual <code>String</code> contains at least on character.
    * @return this assertion object.
    * @throws AssertionError if the actual <code>String</code> is <code>null</code> or empty.
    */
   public StringAssert isNotEmpty() {
-    if (Strings.isEmpty(actual)) 
-      fail(concat(format(description()), "the String ", quote(actual), " should not be empty"));   
+    if (Strings.isEmpty(actual))
+      fail(concat(format(description()), "the String ", quote(actual), " should not be empty"));
     return this;
   }
 
@@ -93,7 +93,7 @@ public final class StringAssert extends GroupAssert<String> {
   public StringAssert isEqualTo(String expected) {
     return (StringAssert)assertEqualTo(expected);
   }
-  
+
   /**
    * Verifies that the actual <code>String</code> is not equal to the given one.
    * @param other the given <code>String</code> to compare the actual <code>String</code> to.
@@ -137,7 +137,7 @@ public final class StringAssert extends GroupAssert<String> {
    * Verifies that the number of characters in the actual <code>String</code> is equal to the given one.
    * @param expected the expected number of characters in the actual <code>String</code>.
    * @return this assertion object.
-   * @throws AssertionError if the number of characters of the actual <code>String</code> is not equal to the given 
+   * @throws AssertionError if the number of characters of the actual <code>String</code> is not equal to the given
    * one.
    */
   public StringAssert hasSize(int expected) {
@@ -155,8 +155,22 @@ public final class StringAssert extends GroupAssert<String> {
    * @throws AssertionError if the actual <code>String</code> does not contain the given one.
    */
   public StringAssert contains(String expected) {
-    if (actual.indexOf(expected) == -1) 
+    if (actual.indexOf(expected) == -1)
       fail(concat(format(description()), "the String ", quote(actual), " should contain the String ", quote(expected)));
+    return this;
+  }
+
+  /**
+   * Verifies that the actual <code>String</code> does not contains the given one.
+   * @param s the given <code>String</code> expected not to be contained in the actual one.
+   * @return this assertion object.
+   * @throws AssertionError if the actual <code>String</code> does contain the given one.
+   */
+  public StringAssert excludes(String s) {
+    if (actual.indexOf(s) != -1)
+      fail(concat(
+          format(description()), "the String ", quote(actual), " should not contain the String ", quote(s)
+      ));
     return this;
   }
 }
--- fest/fest-assert/src/test/java/org/fest/assertions/StringAssertTest.java
+++ fest/fest-assert/src/test/java/org/fest/assertions/StringAssertTest.java
@@ -1,16 +1,16 @@
 /*
  * Created on Jan 10, 2007
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
- * 
+ *
  * Copyright @2007 the original author or authors.
  */
 package org.fest.assertions;
@@ -36,7 +36,7 @@ public class StringAssertTest {
   @Test public void shouldSucceedIfStringIsNull() {
     new StringAssert(null).isEmpty();
   }
-  
+
   @Test(expectedExceptions = AssertionError.class)
   public void shouldFailIfStringsAreNotEqual() {
     new StringAssert("Luke").isEqualTo("Yoda");
@@ -44,7 +44,7 @@ public class StringAssertTest {
 
   @Test public void shouldSucceedIfStringsAreEqual() {
     new StringAssert("Anakin").isEqualTo("Anakin");
-  } 
+  }
 
   @Test(expectedExceptions = AssertionError.class)
   public void shouldFailIfStringDoesNotContainGivenString() {
@@ -53,5 +53,13 @@ public class StringAssertTest {
 
   @Test public void shouldSucceedIfStringContainsGivenString() {
     new StringAssert("Anakin").contains("akin");
-  } 
-}
+  }
+
+  @Test public void shouldSucceedIfStringDoesNotContainGivenString() {
+    new StringAssert("Luke").excludes("Yoda");
+  }
+
+  @Test(expectedExceptions = AssertionError.class)
+   public void shouldFailIfStringContainsGivenString() {
+    new StringAssert("Anakin").excludes("akin");
+  } }
