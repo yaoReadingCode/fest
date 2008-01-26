e68783d7f47ca5eeac99d52c3719c2b6c1083085
wanghy1101 <wanghy1101@b5e4cadd-ff2d-0410-b0d3-97d37c325e25>
2008-01-26 04:27:35 +0000

    Adding formatting for Collections and Maps. Updated project version to 0.3.

--- fest/fest-util/src/test/java/org/fest/util/ArraysTest.java
+++ fest/fest-util/src/test/java/org/fest/util/ArraysTest.java
@@ -1,16 +1,16 @@
 /*
  * Created on May 13, 2007
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
- * 
+ *
  * Copyright @2007 the original author or authors.
  */
 package org.fest.util;
@@ -37,27 +37,35 @@ public class ArraysTest {
   @Test public void shouldReturnIsNotEmptyIfArrayHasElements() {
     assertFalse(Arrays.isEmpty(new String[] { "Tuzi" }));
   }
-  
+
   @Test public void shouldReturnObjectArrayArgument() {
     Object[] array = { "one", "two" };
     assertSame(Arrays.array(array), array);
+  }
+
+  @Test public void shouldReturnNullIfArrayToFormatIsNull() {
+    assertNull(Arrays.format(null));
+  }
+
+  @Test public void shouldReturnEmptyBracketsIfArrayToFormatIsEmpty() {
+    assertEquals(Arrays.format(new Object[0]), "[]");
   }
 
   @Test public void shouldFormatObjectArray() {
     Object o = new Object[] { "First", 3 };
     assertEquals(Arrays.format(o), "['First', 3]");
   }
-  
+
   @Test public void shouldFormatBooleanArray() {
     Object o = new boolean[] { true, false };
     assertEquals(Arrays.format(o), "[true, false]");
   }
-  
+
   @Test public void shouldFormatByteArray() {
     Object o = new byte[] { (byte)3, (byte)8 };
     assertEquals(Arrays.format(o), "[3, 8]");
   }
-  
+
   @Test public void shouldFormatCharArray() {
     Object o = new char[] { 'a', 'b' };
     assertEquals(Arrays.format(o), "[a, b]");
