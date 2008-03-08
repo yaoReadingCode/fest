24447618e43b5a8b39584b0bcd60a99f9237fcef
Alex.Ruiz.05 <Alex.Ruiz.05@b5e4cadd-ff2d-0410-b0d3-97d37c325e25>
2008-03-08 01:07:17 +0000

    Working on panel where user can create new web feed.

---
+++ fest/fest-swing-demo/src/main/java/org/fest/swing/demo/manager/WebFeedManager.java
@@ -0,0 +1,39 @@
+/*
+ * Created on Mar 7, 2008
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ * 
+ * Copyright @2008 the original author or authors.
+ */
+package org.fest.swing.demo.manager;
+
+import org.fest.swing.demo.model.WebFeed;
+
+/**
+ * Understands a manager of web feeds.
+ *
+ * @author Alex Ruiz
+ */
+public interface WebFeedManager {
+  
+  /**
+   * Obtains the name of the feed at the given address.
+   * @param address the address of the web feed.
+   * @return the name of the feed, or <code>null</code> if it could not be found.
+   */
+  String obtainFeedName(String address);
+  
+  /**
+   * Saves the given web feed in the database.
+   * @param webFeed the web feed to save.
+   */
+  void saveWebFeed(WebFeed webFeed);
+}
