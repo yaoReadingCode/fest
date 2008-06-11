682472c07ca9cdd04d332c21358b11f2a091da51
wanghy1101 <wanghy1101@b5e4cadd-ff2d-0410-b0d3-97d37c325e25>
2008-06-11 03:08:33 +0000

    Incomplete - issue 142: [fest-swing] Edit table cells (e.g. check box, text fields etc.) http://code.google.com/p/fest/issues/detail?id=142
    Forgot to commit file.

---
+++ fest/fest-swing/src/test/java/org/fest/swing/driver/TableDialogEditDemoFrame.java
@@ -0,0 +1,41 @@
+/*
+ * Created on Jun 10, 2008
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ * 
+ * Copyright @2008 the original author or authors.
+ */
+package org.fest.swing.driver;
+
+import javax.swing.JTable;
+
+import org.fest.swing.testing.TableDialogEditDemo;
+import org.fest.swing.testing.TestFrame;
+
+/**
+ * Understands a frame hosting <code>{@link TableDialogEditDemo}</code>.
+ *
+ * @author Yvonne Wang
+ * @author Alex Ruiz
+ */
+class TableDialogEditDemoFrame extends TestFrame {
+  private static final long serialVersionUID = 1L;
+
+  final JTable table;
+
+  public TableDialogEditDemoFrame() {
+    super(BasicJTableCellWriterTest.class);
+    TableDialogEditDemo newContentPane = new TableDialogEditDemo();
+    table = newContentPane.table;
+    newContentPane.setOpaque(true); // content panes must be opaque
+    setContentPane(newContentPane);
+  }
+}
