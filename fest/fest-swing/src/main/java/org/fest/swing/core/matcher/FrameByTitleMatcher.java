ad8f0fb9ef9a1a0c66ec48c0d8cfa2d3d98f5b0b
alex.ruiz.05 <alex.ruiz.05@b5e4cadd-ff2d-0410-b0d3-97d37c325e25>
2008-07-17 06:11:06 +0000

    Complete - issue 158: [fest-swing] ability to set global component matcher http://code.google.com/p/fest/issues/detail?id=158
    Added FrameByTitleMatcher.

---
+++ fest/fest-swing/src/main/java/org/fest/swing/core/matcher/FrameByTitleMatcher.java
@@ -0,0 +1,70 @@
+/*
+ * Created on Jul 16, 2008
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ * 
+ * Copyright @2008 the original author or authors.
+ */
+package org.fest.swing.core.matcher;
+
+import java.awt.Frame;
+
+import org.fest.swing.core.GenericTypeMatcher;
+
+/**
+ * Understands matching a <code>{@link Frame}</code> whose title is equal to the provided one.
+ *
+ * @author Alex Ruiz 
+ */
+public class FrameByTitleMatcher extends GenericTypeMatcher<Frame> {
+
+  /**
+   * Creates a new <code>{@link FrameByTitleMatcher}</code> that matches a <code>{@link Frame}</code> whose title is
+   * equal to the provided one. The <code>{@link Frame}</code> to match does not have to be showing.
+   * @param title the expected title.
+   * @return the created matcher.
+   */
+  public static FrameByTitleMatcher withTitle(String title) {
+    return new FrameByTitleMatcher(title);
+  }
+  
+  /**
+   * Creates a new <code>{@link FrameByTitleMatcher}</code> that matches a <code>{@link Frame}</code> whose title is
+   * equal to the provided one. The <code>{@link Frame}</code> <strong>should</strong> be showing.
+   * @param title the expected title.
+   * @return the created matcher.
+   */
+  public static FrameByTitleMatcher withTitleAndShowing(String title) {
+    return new FrameByTitleMatcher(title, true);
+  }
+  
+  private final String title;
+
+  private FrameByTitleMatcher(String title) {
+    this.title = title;
+  }
+
+  private FrameByTitleMatcher(String title, boolean requireShowing) {
+    super(requireShowing);
+    this.title = title;
+  }
+
+  /** 
+   * Indicates whether the title of the given <code>{@link Frame}</code> is equal to the one specified in this matcher.
+   * @param frame the <code>Frame</code> to verify.
+   * @return <code>true</code> if the title of the <code>Frame</code> is equal to the one specified in this matcher,
+   * otherwise <code>false</code>. 
+   */
+  protected boolean isMatching(Frame frame) {
+    return frame.getTitle().equals(title);
+  }
+
+}
---
+++ fest/fest-swing/src/test/java/org/fest/swing/core/matcher/FrameByTitleMatcherTest.java
@@ -0,0 +1,84 @@
+/*
+ * Created on Jul 16, 2008
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ * 
+ * Copyright @2008 the original author or authors.
+ */
+package org.fest.swing.core.matcher;
+
+import javax.swing.JFrame;
+
+import org.testng.annotations.Test;
+
+import org.fest.swing.testing.TestFrame;
+
+import static org.fest.assertions.Assertions.assertThat;
+import static org.fest.swing.testing.TestGroups.GUI;
+
+/**
+ * Tests for <code>{@link FrameByTitleMatcher}</code>.
+ *
+ * @author Alex Ruiz
+ */
+@Test public class FrameByTitleMatcherTest {
+
+  public void shouldReturnTrueIfTitleIsEqualToExpected() {
+    String title = "Hello";
+    FrameByTitleMatcher matcher = FrameByTitleMatcher.withTitle(title);
+    JFrame frame = new JFrame(title);
+    assertThat(matcher.matches(frame)).isTrue();
+  }
+  
+  public void shouldReturnFalseIfTitleIsNotEqualToExpected() {
+    FrameByTitleMatcher matcher = FrameByTitleMatcher.withTitle("Hello");
+    JFrame frame = new JFrame("Bye");
+    assertThat(matcher.matches(frame)).isFalse();
+  }
+  
+  @Test(groups = GUI)
+  public void shouldReturnTrueIfFrameIsShowingAndTitleIsEqualToExpected() {
+    Class<FrameByTitleMatcher> testType = FrameByTitleMatcher.class;
+    TestFrame frame = new TestFrame(testType);
+    try {
+      frame.display();
+      FrameByTitleMatcher matcher = FrameByTitleMatcher.withTitleAndShowing(testType.getName());
+      assertThat(matcher.matches(frame)).isFalse();
+    } finally {
+      frame.destroy();
+    }
+  }
+  
+  public void shouldReturnFalseIfFrameIsNotShowingAndTitleIsEqualToExpected() {
+    String title = "Hello";
+    FrameByTitleMatcher matcher = FrameByTitleMatcher.withTitleAndShowing(title);
+    JFrame frame = new JFrame(title);
+    assertThat(matcher.matches(frame)).isFalse();    
+  }
+
+  @Test(groups = GUI)
+  public void shouldReturnFalseIfFrameIsShowingAndTitleIsNotEqualToExpected() {
+    TestFrame frame = new TestFrame(FrameByTitleMatcher.class);
+    try {
+      frame.display();
+      FrameByTitleMatcher matcher = FrameByTitleMatcher.withTitleAndShowing("Hello");
+      assertThat(matcher.matches(frame)).isFalse();
+    } finally {
+      frame.destroy();
+    }
+  }
+
+  public void shouldReturnFalseIfFrameIsNotShowingAndTitleIsNotEqualToExpected() {
+    FrameByTitleMatcher matcher = FrameByTitleMatcher.withTitleAndShowing("Hello");
+    JFrame frame = new JFrame("Bye");
+    assertThat(matcher.matches(frame)).isFalse();    
+  }
+}
