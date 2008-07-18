e862343051abe68426ea9eb10e3bb8332c78f7f2
wanghy1101 <wanghy1101@b5e4cadd-ff2d-0410-b0d3-97d37c325e25>
2008-07-18 04:29:03 +0000

    Complete - issue 177: [fest-swing] JTableCellFixture#enterValue(String) with JComboBox as editor http://code.google.com/p/fest/issues/detail?id=177
    Fixed.

--- fest/fest-swing/src/main/java/org/fest/swing/driver/JTableComboBoxEditorCellWriter.java
+++ fest/fest-swing/src/main/java/org/fest/swing/driver/JTableComboBoxEditorCellWriter.java
@@ -1,19 +1,21 @@
 /*
  * Created on Jun 10, 2008
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
- * 
+ *
  * Copyright @2008 the original author or authors.
  */
 package org.fest.swing.driver;
+
+import static org.fest.swing.core.Pause.pause;
 
 import java.awt.Component;
 
@@ -26,7 +28,7 @@ import org.fest.swing.core.Robot;
 /**
  * Understands an implementation of <code>{@link JTableCellWriter}</code> that knows how to use
  * <code>{@link JComboBox}</code>es as cell editors.
- * 
+ *
  * @author Alex Ruiz
  * @author Yvonne Wang
  */
@@ -42,18 +44,15 @@ public class JTableComboBoxEditorCellWriter extends AbstractJTableCellWriter {
   /** ${@inheritDoc} */
   public void enterValue(JTable table, int row, int column, String value) {
     JComboBox editor = editor(table, row, column);
-    startEditing(table, row, column);
+    startCellEditing(table, row, column);
     driver.selectItem(editor, value);
   }
 
   /** ${@inheritDoc} */
   public void startCellEditing(JTable table, int row, int column) {
-    editor(table, row, column);
-    startEditing(table, row, column);
-  }
-
-  private void startEditing(JTable table, int row, int column) {
+    JComboBox editor = editor(table, row, column);
     clickCell(table, row, column);
+    pause(new WaitForComponentToShowCondition(editor));
   }
 
   /** ${@inheritDoc} */
