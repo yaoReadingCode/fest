4657d89539a01bff97915f4bfb0de4f678289791
alex.ruiz.05 <alex.ruiz.05@b5e4cadd-ff2d-0410-b0d3-97d37c325e25>
2008-07-25 03:14:31 +0000

    Added test case for issue 186.

---
+++ fest/fest-swing/src/test/java/org/fest/swing/fixture/ReadSortedTableTest.java
@@ -0,0 +1,103 @@
+/*
+ * Created on Jul 24, 2008
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ * 
+ * Copyright @2008 the original author or authors.
+ */
+package org.fest.swing.fixture;
+
+import java.awt.Dimension;
+
+import javax.swing.JTextField;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.table.TableModel;
+import javax.swing.table.TableRowSorter;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import org.fest.swing.core.GuiTask;
+import org.fest.swing.testing.TestTable;
+import org.fest.swing.testing.TestWindow;
+
+import static javax.swing.RowFilter.regexFilter;
+
+import static org.fest.assertions.Assertions.assertThat;
+import static org.fest.swing.fixture.TableCell.row;
+import static org.fest.swing.testing.TestGroups.*;
+
+/**
+ * Tests for <a href="http://code.google.com/p/fest/issues/detail?id=186" target="_blank">issue 186</a>.
+ *
+ * @author Alex Ruiz
+ */
+@Test(groups = { GUI, BUG })
+public class ReadSortedTableTest {
+
+  private FrameFixture frame;
+  
+  @BeforeMethod public void setUp() {
+    MyFrame window = new GuiTask<MyFrame>() {
+      protected MyFrame executeInEDT() {
+        return new MyFrame();
+      }
+    }.run();
+    frame = new FrameFixture(window);
+    frame.show();
+  }
+  
+  @AfterMethod public void tearDown() {
+    frame.cleanUp();
+  }
+  
+  public void shouldReadFirstCellAfterReading() {
+    frame.textBox("textBox").enterText("2");
+    assertThat(frame.table("table").valueAt(row(0).column(0))).isEqualTo("2-0");
+  }
+  
+  private static class MyFrame extends TestWindow {
+    private static final long serialVersionUID = 1L;
+    
+    final TableRowSorter<TableModel> sorter = new TableRowSorter<TableModel>();
+
+    MyFrame() {
+      super(ReadSortedTableTest.class);
+      add(textBox());
+      add(table());
+      setPreferredSize(new Dimension(200, 200));
+    }
+
+    private JTextField textBox() {
+      final JTextField textBox = new JTextField(20);
+      textBox.getDocument().addDocumentListener(new DocumentListener() {
+        public void insertUpdate(DocumentEvent e) { filterTable(); }
+        public void removeUpdate(DocumentEvent e) { filterTable();}
+        public void changedUpdate(DocumentEvent e) { }
+
+        private void filterTable() {
+          sorter.setRowFilter(regexFilter(textBox.getText(), 0));
+        }
+      });
+      textBox.setName("textBox");
+      return textBox;
+    }
+    
+    private TestTable table() {
+      TestTable table = new TestTable("table", 5, 2);
+      sorter.setModel(table.getModel());
+      table.setRowSorter(sorter);
+      return table;
+    }
+  }
+}
