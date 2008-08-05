4ea15afb1f919252e49385965b30200107b82dde
wanghy1101 <wanghy1101@b5e4cadd-ff2d-0410-b0d3-97d37c325e25>
2008-08-05 14:11:27 +0000

    Reduced some code duplication.

--- fest/fest-swing/src/main/java/org/fest/swing/driver/JScrollBarLocationStrategy.java
+++ fest/fest-swing/src/main/java/org/fest/swing/driver/JScrollBarLocationStrategy.java
@@ -1,16 +1,16 @@
 /*
  * Created on Jul 31, 2008
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
- * 
+ *
  * Copyright @2008 the original author or authors.
  */
 package org.fest.swing.driver;
@@ -25,18 +25,23 @@ import org.fest.swing.core.GuiTask;
  * Understands encapsulation of a location in a <code>{@link JScrollBar}</code> in a orientation-specific way.
  *
  * @author Alex Ruiz
+ * @author Yvonne Wang
  */
-interface JScrollBarLocationStrategy {
+abstract class JScrollBarLocationStrategy {
 
-  int arrow(JScrollBar scrollBar);
-  
-  Point thumbLocation(JScrollBar scrollBar, double fraction);
-  
-  Point blockLocation(JScrollBar scrollBar, Point unitLocation, int offset);
-  
-  Point unitLocationToScrollUp(JScrollBar scrollBar);
-  
-  static class GetWidthTask extends GuiTask<Integer> {
+  abstract int arrow(JScrollBar scrollBar);
+
+  abstract Point thumbLocation(JScrollBar scrollBar, double fraction);
+
+  abstract Point blockLocation(JScrollBar scrollBar, Point unitLocation, int offset);
+
+  abstract Point unitLocationToScrollUp(JScrollBar scrollBar);
+
+  final int widthOf(final JScrollBar scrollBar) {
+    return new GetWidthTask(scrollBar).run();
+  }
+
+  private static class GetWidthTask extends GuiTask<Integer> {
     private final JScrollBar scrollBar;
 
     GetWidthTask(JScrollBar scrollBar) {
@@ -47,8 +52,12 @@ interface JScrollBarLocationStrategy {
       return scrollBar.getWidth();
     }
   }
-  
-  static class GetHeightTask extends GuiTask<Integer> {
+
+  final int heightOf(final JScrollBar scrollBar) {
+    return new GetHeightTask(scrollBar).run();
+  }
+
+  private static class GetHeightTask extends GuiTask<Integer> {
     private final JScrollBar scrollBar;
 
     GetHeightTask(JScrollBar scrollBar) {
--- fest/fest-swing/src/main/java/org/fest/swing/driver/VerticalJScrollBarLocationStrategy.java
+++ fest/fest-swing/src/main/java/org/fest/swing/driver/VerticalJScrollBarLocationStrategy.java
@@ -1,16 +1,16 @@
 /*
  * Created on Jul 31, 2008
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
- * 
+ *
  * Copyright @2008 the original author or authors.
  */
 package org.fest.swing.driver;
@@ -22,12 +22,13 @@ import javax.swing.JScrollBar;
 /**
  * Understands encapsulation of a location in a horizontal <code>{@link JScrollBar}</code>.
  *
- * @author Alex Ruiz 
+ * @author Alex Ruiz
+ * @author Yvonne Wang
  */
-class VerticalJScrollBarLocationStrategy implements JScrollBarLocationStrategy {
-  
+class VerticalJScrollBarLocationStrategy extends JScrollBarLocationStrategy {
+
   public int arrow(JScrollBar scrollBar) {
-    return new GetWidthTask(scrollBar).run();
+    return widthOf(scrollBar);
   }
 
   public Point thumbLocation(JScrollBar scrollBar, double fraction) {
@@ -44,9 +45,5 @@ class VerticalJScrollBarLocationStrategy implements JScrollBarLocationStrategy {
   public Point unitLocationToScrollUp(JScrollBar scrollBar) {
     int arrow = arrow(scrollBar);
     return new Point(arrow / 2, heightOf(scrollBar) - arrow / 2);
-  }
-
-  private int heightOf(final JScrollBar scrollBar) {
-    return new GetHeightTask(scrollBar).run();
   }
 }
