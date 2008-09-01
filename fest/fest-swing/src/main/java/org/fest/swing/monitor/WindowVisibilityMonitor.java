bc672610cb6943c41a3a4e288ed161c4e0aaf765
wanghy1101 <wanghy1101@b5e4cadd-ff2d-0410-b0d3-97d37c325e25>
2008-09-01 19:01:42 +0000

    Minor changes.

--- fest/fest-swing/src/main/java/org/fest/swing/monitor/WindowVisibilityMonitor.java
+++ fest/fest-swing/src/main/java/org/fest/swing/monitor/WindowVisibilityMonitor.java
@@ -1,16 +1,16 @@
 /*
  * Created on Oct 8, 2007
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
- * 
+ *
  * Copyright @2007-2008 the original author or authors.
  */
 package org.fest.swing.monitor;
@@ -49,6 +49,7 @@ class WindowVisibilityMonitor extends WindowAdapter implements ComponentListener
 
   @Override public void windowClosed(WindowEvent e) {
     Window w = e.getWindow();
+    // already in EDT
     w.removeComponentListener(this);
     w.removeWindowListener(this);
   }
