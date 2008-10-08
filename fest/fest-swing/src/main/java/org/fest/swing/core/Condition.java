09802db98ee2a254661a8003bdb5b28cdd826dbc
wanghy1101 <wanghy1101@b5e4cadd-ff2d-0410-b0d3-97d37c325e25>
2008-10-08 15:16:20 +0000

    Minor changes.

--- fest/fest-swing/src/main/java/org/fest/swing/core/Condition.java
+++ fest/fest-swing/src/main/java/org/fest/swing/core/Condition.java
@@ -1,15 +1,14 @@
 /*
  * Created on Apr 14, 2007
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
- * in compliance with the License. You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software distributed under the License
- * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
- * or implied. See the License for the specific language governing permissions and limitations under
- * the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  *
  * Copyright @2007-2008 the original author or authors.
  */
@@ -42,12 +41,15 @@ public abstract class Condition {
    * Returns the <code>String</code> representation of this condition, which is its description.
    * @return the description of this condition.
    */
-  @Override public final String toString() {  return description; }
-
+  @Override
+  public final String toString() {
+    return description;
+  }
 
   /**
-   * Notification that this condition has been evaluated (regardless of the outcome.) This is a good place to do any
-   * necessary resource cleanup.
+   * Notification that this condition has been evaluated This method is invoked by
+   * <code>{@link Pause#pause(Condition)}</code> (and all overloaded methods) when a condition is evaluated (either the
+   * condition was satisfied or it timed-out.) This is a good place to do any necessary resource cleanup.
    */
   protected void done() {}
 }
