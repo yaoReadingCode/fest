1f9396ff58b5d3a7ba4de6a00d63656c1ecbae60
wanghy1101 <wanghy1101@b5e4cadd-ff2d-0410-b0d3-97d37c325e25>
2008-10-15 14:57:57 +0000

    Fixed failing tests.

--- fest/fest-swing/src/main/java/org/fest/swing/input/DragAwareEventNormalizer.java
+++ fest/fest-swing/src/main/java/org/fest/swing/input/DragAwareEventNormalizer.java
@@ -1,16 +1,16 @@
 /*
  * Created on Jun 25, 2008
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
- * 
+ *
  * Copyright @2008 the original author or authors.
  */
 package org.fest.swing.input;
@@ -48,9 +48,15 @@ class DragAwareEventNormalizer extends EventNormalizer {
       toolkit.getSystemEventQueue().push(dragAwareEventQueue);
       // execute(pushDragAwareEventQueueTask(toolkit, dragAwareEventQueue));
     } catch (UnexpectedException e) {
-      String message = concat("Ignoring error when starting up ", DragAwareEventNormalizer.class.getName());
-      logger.log(WARNING, message, e);
+      ignore(e);
+    } catch (RuntimeException e) {
+      ignore(e);
     }
+  }
+
+  private void ignore(Exception e) {
+    String message = concat("Ignoring error when starting up ", DragAwareEventNormalizer.class.getName());
+    logger.log(WARNING, message, e);
   }
 
   DragAwareEventQueue newDragAwareEventQueue(Toolkit toolkit, long mask) {
